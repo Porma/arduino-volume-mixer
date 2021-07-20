e1b16aabdb477b47192fdb40c08d735e6bdecb99
Pramodh <pormahd@gmail.com>
2021-07-20 21:17:01 +1000

    added serial comms for buttons

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -4,9 +4,36 @@
 Encoder enc[] = {Encoder(3,2), Encoder(6,5)};
 int encSize = sizeof(enc)/sizeof(Encoder);
 
+// smol class for encoder button pin and state
+class Button 
+{
+  public:
+    Button();
+    int pin;
+    bool pressed;
+    
+  Button(int pin)
+  {
+    this->pin = pin;
+    this->pressed = false;
+  }
+};
+
+// gotta put this in the same order as encoder array
+// e.g Button(4) is the button on Encoder(3,2)
+// hopefully encSize will be reliable enough to use for this also
+Button btn[] = {Button(4), Button(7)};
+
+
+bool pressed = false;
+
 void setup()
 {
   Serial.begin(115200); //open a serial port
+  
+  for (int i = 0; i < encSize; i++) {
+    pinMode(btn[i].pin, INPUT_PULLUP);
+  }
 }
 
 void loop()
@@ -37,6 +64,21 @@ void loop()
 
       // Reset encoder value
       enc[i].write(0);
+    }
+  }
+
+  // Loop through buttons
+  for (int i = 0; i < encSize; i++) {
+    // if read button press and state was not pressed
+    if (digitalRead(btn[i].pin) == LOW && !btn[i].pressed) {
+      Serial.print(String(i) + "2");
+      btn[i].pressed = true;
+    }
+
+    // if read button release and state was pressed
+    if (digitalRead(btn[i].pin) == HIGH && btn[i].pressed) {
+      Serial.print(String(i) + "3");
+      btn[i].pressed = false;
     }
   }
   
